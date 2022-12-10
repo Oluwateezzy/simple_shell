f7a339b527fd22022e7263816c2bb1dcd5e271a7
oluwatobiloba <oluwateezzy03@gmail.com>
2022-12-10 05:09:57 -0800

    alx commit

--- prompt.c
+++ prompt.c
@@ -5,16 +5,21 @@
  */
 void prompt()
 {
-	char *line;
+	char *line, *parsedArgs[100], *parsed_piped[1000];
 	char **args;
 	int status;
+	int exec_flag = 0;
 
 	do
 	{
 		printf("valerie&tobi $ ");
 		line = read_line();
-		args = split_line(line);
-		status = execute(args);
+		exec_flag = process_string(#line, parsedArgs, parsed_piped);
+		if (exec_flag == 1)
+		{
+			args = split_line(line);
+			status = execute(args);
+		}
 
 		free(line);
 		free(args);
